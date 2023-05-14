0849cd21d9fb2e707890e7498454f76501810465
bohdansharaiev <129484207+bohdansharaiev@users.noreply.github.com>
2023-05-14 17:51:59 +0300

    Add files via upload

--- курсовагидро/Form1.Designer.cs
+++ курсовагидро/Form1.Designer.cs
@@ -32,29 +32,37 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(79, 32);
+            this.comboBox1.Location = new System.Drawing.Point(56, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(206, 21);
             this.comboBox1.TabIndex = 0;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(44, 91);
+            this.richTextBox1.Location = new System.Drawing.Point(37, 56);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(282, 227);
+            this.richTextBox1.Size = new System.Drawing.Size(250, 207);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(357, 32);
+            this.button1.Location = new System.Drawing.Point(84, 284);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 67);
             this.button1.TabIndex = 2;
@@ -65,16 +73,97 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(394, 114);
+            this.dataGridView1.Location = new System.Drawing.Point(558, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(354, 237);
+            this.dataGridView1.Size = new System.Drawing.Size(216, 237);
             this.dataGridView1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(337, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(337, 115);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(337, 147);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(337, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 258);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(558, 258);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 33);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(352, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 33);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
@@ -84,6 +173,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,6 +183,14 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- курсовагидро/Form1.cs
+++ курсовагидро/Form1.cs
@@ -4,38 +4,29 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static курсовагидро.Form1;
 
 namespace курсовагидро
 {
+   
     public partial class Form1 : Form
     {
-        private List<River> rivers = new List<River>();
-        private List<Lake> lakes = new List<Lake>();
-        private List<Sea> seas = new List<Sea>();
+        Form2 frm2;
+      
         public Form1()
         {
             InitializeComponent();
-
-
-
-        }
-       
-        public class River
-        {
-            public string Name { get; set; }
-            public int Length { get; set; }
-            public bool Vpad { get; set; }
-            public double Flow { get; set; }
-            public double BasinArea { get; set; }
-            public string Location { get; set; }
-            public bool Navigable { get; set; }
-            public string Countries { get; set; }
         }
 
+      
+        public List<Lake> lakes = new List<Lake>();
+        public List<Sea> seas = new List<Sea>();
+        
         public class Lake
         {
             public string Name { get; set; }
@@ -65,9 +56,7 @@ namespace курсовагидро
          
 
             // Додати річки до списку
-            rivers.Add(new River { Name = "Дніпро", Length = 2200, Vpad = true, Flow = 1738, BasinArea = 504000, Location = "Europe", Navigable = true, Countries = "Ukraine, Russia, Belarus" });
-            rivers.Add(new River { Name = "Дунай", Length = 2860, Vpad = true, Flow = 2000, BasinArea = 817000, Location = "Europe", Navigable = true, Countries = "Germany, Austria, Slovakia, Hungary, Croatia, Serbia, Romania, Bulgaria, Ukraine" });
-            rivers.Add(new River { Name = "Волга", Length = 3530, Vpad = false, Flow = 2500, BasinArea = 1390000, Location = "Europe", Navigable = true, Countries = "Russia" });
+           
 
             lakes.Add(new Lake { Name = "Венеція", Length = 54, Depth = 5, Volume = 35.5, Countries = "Italy" });
             lakes.Add(new Lake { Name = "Байкал", Length = 636, Depth = 1637, Volume = 23600, Countries = "Russia" });
@@ -79,17 +68,7 @@ namespace курсовагидро
 
 
 
-            dataGridView1.Columns.Add("Name", "Назва");
-            dataGridView1.Columns.Add("Area", "Площа (км²)");
-            dataGridView1.Columns.Add("Vpad", "Впадає");
-            dataGridView1.Columns.Add("Depth", "Глибина (км²)");
-            dataGridView1.Columns.Add("Location", "Місце");
-            dataGridView1.Columns.Add("Flow", "Flow (км²)");
-            dataGridView1.Columns["Name"].Width = 150;
-            dataGridView1.Columns["Area"].Width = 100;
-            dataGridView1.Columns["Vpad"].Width = 100;
-            dataGridView1.Columns["Depth"].Width = 100;
-            dataGridView1.Columns["Location"].Width = 100;
+          
 
             // Додати назви морів до комбо-боксу та рядки до таблиці
 
@@ -101,243 +80,75 @@ namespace курсовагидро
             comboBox1.Items.Add("Моря");
             
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        DataTable table = new DataTable();
+        public void Print(List<River> list)
         {
+            dataGridView1.DataSource = null;
+            table.Rows.Clear();
+            table.Columns.Clear();
+
+            table.Columns.Add("Назва", typeof(string));
+            table.Columns.Add("Страна", typeof(string));
+            table.Columns.Add("Довжина", typeof(int));
            
-            dataGridView1.Rows.Clear();
-
-
-
-
-
-
-         
-           
-
-            if (comboBox1.SelectedItem != null)
+            table.Columns.Add("Flow", typeof(double));
+            
+            foreach (River river in list)
             {
-                string selectedWaterBody = comboBox1.SelectedItem.ToString();
-                // Ваш код здесь
-
-
-                if (selectedWaterBody == "Річки")
-                {
-                    foreach (River river in rivers)
-                    {
-                        if (checkBox1.Checked)
-                        {
-                            // Перевірити, чи впадає річка в море
-
-                            // Вивести інформацію про всі річки в RichTextBox
-
-
-                            if (river.Vpad == true)
-                            {
-                                dataGridView1.Rows.Add(river.Name, river.Length, river.Vpad, river.Location);
-                            }
-
-                        }
-                        else
-                        {
-                            dataGridView1.Rows.Add(river.Name, river.Length, river.Vpad, river.Location);
-                        }
-                    }
-                }
-                if (selectedWaterBody == "Озера")
-                {
-
-                    foreach (Lake lake in lakes)
-                    {
-                        dataGridView1.Rows.Add(lake.Name, lake.Length, lake.Countries);
-                    }
-                }
-                if (selectedWaterBody == "Моря")
-                {
-                    foreach (Sea sear in seas)
-                    {
-                        dataGridView1.Rows.Add(sear.Name, sear.Length, sear.Vpad, sear.Countries);
-                    }
-                }
-                if (selectedWaterBody == "Річки")
-                {
-                    // Вивести інформацію про всі річки в RichTextBox
-
-                    // Перевірити, чи впадає річка в море
-
-                    // Вивести інформацію про всі річки в RichTextBox
-                    richTextBox1.Clear();
-                    foreach (River river in rivers)
-                    {
-                        if (checkBox1.Checked)
-                        {
-                            if (river.Vpad == true)
-                            {
-                                richTextBox1.AppendText("Назва: " + river.Name + "\n");
-                                richTextBox1.AppendText("Довжина: " + river.Length.ToString() + " км\n");
-                            }
-                        }
-                        else
-                        {
-                            richTextBox1.AppendText("Назва: " + river.Name + "\n");
-                            richTextBox1.AppendText("Довжина: " + river.Length.ToString() + " км\n");
-                        }
-                    }
-                }
-                else if (selectedWaterBody == "Озера")
-                {
-                    // Вивести інформацію про всі озера в RichTextBox
-                    richTextBox1.Clear();
-                    foreach (Lake lake in lakes)
-                    {
-                        richTextBox1.AppendText("Назва: " + lake.Name + "\n");
-                        richTextBox1.AppendText("Довжина: " + lake.Length.ToString() + " км\n");
-                    }
-                }
-                else if (selectedWaterBody == "Моря")
-                {
-                    // Вивести інформацію про всі моря в RichTextBox
-                    richTextBox1.Clear();
-                    foreach (Sea sea in seas)
-                    {
-                        if (checkBox2.Checked)
-                        {
-                            if (sea.Vpad == true)
-                            {
-
-
-                                richTextBox1.AppendText("Назва: " + sea.Name + "\n");
-                            }
-                        }
-                        else
-                        {
-                            richTextBox1.AppendText("Назва: " + sea.Name + "\n");
-                        }
-                    }
-                }
+                table.Rows.Add(river.Name, river.Length, river.Flow, river.Countries);
                 
             }
-            else
-            {
-                MessageBox.Show("Необходимо выбрать водное тело.");
-            }
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            //dataGridView1.AutoResizeColumns();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders);
+
+            dataGridView1.DataSource = table;
+            dataGridView1.Refresh();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+      
+        private void button1_Click(object sender, EventArgs e)
         {
-            string selectedWaterBody = comboBox1.SelectedItem.ToString();
-            if (selectedWaterBody == "Річки")
-            {
-                string name = textBox1.Text;
-                River newRiver = new River { Name = name };
+            Print(Rivers.rivers);
+            
+        }
+       
+        private void button4_Click(object sender, EventArgs e)
+        {
+             frm2 = new Form2();
+            frm2.Show();
 
-                rivers.Add(newRiver);
-            }
-            else if (selectedWaterBody == "Озера")
-            {
-                string name = textBox1.Text;
-                Lake newLake = new Lake { Name = name };
-
-                lakes.Add(newLake);
-            }
-            else if (selectedWaterBody == "Моря")
-            {
-
-                string name = textBox1.Text;
-                Sea newSea = new Sea { Name = name };
-
-                seas.Add(newSea);
-            }
-            }
+        }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string selectedWaterBody = comboBox1.SelectedItem.ToString();
-            if (selectedWaterBody == "Річки")
+            string name = textBox2.Text;
+            River riverToDelete = null;
+            foreach (River river in Rivers.rivers)
             {
-                string name = textBox2.Text;
-
-                River riverToDelete = null;
-                foreach (River river in rivers)
+                if (river.Name == name)
                 {
-                    if (river.Name == name)
-                    {
-                        riverToDelete = river;
-                        break;
-                    }
-                }
-
-                if (riverToDelete != null)
-                {
-                    rivers.Remove(riverToDelete);
-                    dataGridView1.Rows.Clear();
-                    foreach (River river in rivers)
-                    {
-                        dataGridView1.Rows.Add(river.Name);
-                    }
+                    riverToDelete = river;
+                    break;
                 }
             }
 
-
-
-            string selectedWaterBody2 = comboBox1.SelectedItem.ToString();
-            if (selectedWaterBody2 == "Озера")
+            if (riverToDelete != null)
             {
-                string name = textBox2.Text;
-
-                Lake lakeToDelete = null;
-                foreach (Lake lake in lakes)
+                Rivers.rivers.Remove(riverToDelete);
+                dataGridView1.Rows.Clear();
+                foreach (River river in Rivers.rivers)
                 {
-                    if (lake.Name == name)
-                    {
-                        lakeToDelete = lake;
-                        break;
-                    }
-                }
-
-                if (lakeToDelete != null)
-                {
-                    lakes.Remove(lakeToDelete);
-                    dataGridView1.Rows.Clear();
-                    foreach (Lake lake in lakes)
-                    {
-                        dataGridView1.Rows.Add(lake.Name);
-                    }
-                }
-            }
-            string selectedWaterBody3 = comboBox1.SelectedItem.ToString();
-            if (selectedWaterBody3 == "Моря")
-            {
-                string name = textBox2.Text;
-
-                Sea seaToDelete = null;
-                foreach (Sea sea in seas)
-                {
-                    if (sea.Name == name)
-                    {
-                        seaToDelete = sea;
-                        break;
-                    }
-                }
-
-                if (seaToDelete != null)
-                {
-                    seas.Remove(seaToDelete);
-                    dataGridView1.Rows.Clear();
-                    foreach (Sea sea in seas)
-                    {
-                        dataGridView1.Rows.Add(sea.Name);
-                    }
+                    dataGridView1.Rows.Add(river.Name);
                 }
             }
         }
     }
    
 }
-
-
-
-
 
 
 
