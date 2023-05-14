44e42cd0d33d72c1b4e20ea50fc86d7154663878
bohdansharaiev <129484207+bohdansharaiev@users.noreply.github.com>
2023-05-14 18:32:20 +0300

    Add files via upload

---
+++ курсовагидро/Listlake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсовагидро
+{
+    internal class Listlake
+    {
+    }
+    public class Lake
+    {
+        public string Name;
+        public int Length;
+        public double Flow;
+
+        public string Countries;
+        public Lake(string name, int length, double flow, string countries)
+        {
+            Name = name;
+            Length = length;
+            Flow = flow;
+            Countries = countries;
+        }
+    }
+
+    public static class Lakes
+    {
+        public static bool fl = true;
+        public static List<Lake> lakes = new List<Lake>();
+
+        public static void Add(Lake Lake)
+        {
+            lakes.Add(Lake);
+        }
+
+    }
+}
